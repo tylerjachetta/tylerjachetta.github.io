ffd96b0b196c37680fc85221d6b398ded4aaee55
tylerjachetta <jachetta.tyler@gmail.com>
2016-09-01 14:49:58 -0700

    test

--- emily/assets/js/custom.js
+++ emily/assets/js/custom.js
@@ -196,10 +196,43 @@
 		 * Gallery
 		/* ---------------------------------------------- */
 
-		$('.gallery-item a').magnificPopup({
-			type: 'image',
-			gallery: { enabled: true },
-		});
+		$(document).ready(function() {
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+
+	$('.image-popup-fit-width').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		image: {
+			verticalFit: false
+		}
+	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
+});
+	
 
 		/* ---------------------------------------------- /*
 		 * A jQuery plugin for fluid width video embeds
