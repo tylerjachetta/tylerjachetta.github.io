a66ebbdc011ed1afce0c447d30f7962f07e4183b
tylerjachetta <jachetta.tyler@gmail.com>
2017-04-07 15:50:52 -0700

    test

--- Emishly.github.io/assets/js/custom.js
+++ Emishly.github.io/assets/js/custom.js
@@ -299,8 +299,16 @@
 				ajaxLoad(workNumberToload, pageNumber);
 			}, 300);
 
-			pageNumber++;
-
+			pageNumber++;	
+			$('.image-popup-vertical-fit').magnificPopup({
+				type: 'image',
+				closeOnContentClick: true,
+				mainClass: 'mfp-img-mobile',
+				image: {
+					verticalFit: true
+				}
+				
+			});
 			return false;
 		});
 
